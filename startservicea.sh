79f0693c02d28c60c4caeb6a3e933c9fe39408a2
chengzhang <676931854@qq.com>
2022-03-14 14:53:10 +0800

    FIX

--- startservicea.sh
+++ startservicea.sh
@@ -14,7 +14,7 @@ echo 'kill旧的服务完成'
  
 echo '启动项目'
 
-nohup java -jar  -Xms128m -Xmx128m --spring.profiles.active=a1 /root/DiscoveryGuide/$name/target/$name-1.0.0.jar > log.out  2>&1 &
+nohup java -jar  -Xms128m -Xmx128m -Dspring.profiles.active=a1 /root/DiscoveryGuide/$name/target/$name-1.0.0.jar > log.out  2>&1 &
 
 echo "tail -f /root/logs/$name/info.log"
 echo '脚本执行结束，请等待项目启动'
