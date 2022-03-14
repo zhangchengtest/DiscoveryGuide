3c5fd5222ee91fdd9839c3b9b551dac5d6d7c8a9
chengzhang <676931854@qq.com>
2022-03-14 15:07:55 +0800

    fix bug

--- startgate.sh
+++ startgate.sh
@@ -16,6 +16,6 @@ echo '启动项目'
 
 nohup java -jar  -Xms128m -Xmx128m -Dreactor.netty.pool.leasingStrategy=lifo /root/DiscoveryGuide/$name/target/$name-1.0.0.jar > gate.out  2>&1 &
 
-echo "tail -f /root/logs/$name/info.log"
+echo "tail -f /root/DiscoveryGuide/gate.out"
 echo '脚本执行结束，请等待项目启动'
-tail -f /root/logs/$name/info.log
+tail -f /root/DiscoveryGuide/gate.out
--- startservicea.sh
+++ startservicea.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-name=discovery-guide-gateway
+name=discovery-guide-service
 echo "kill旧的服务 $name"
 msg=`ps -ef | grep $name |grep -v 'grep'`
 echo "$msg";
@@ -16,6 +16,6 @@ echo '启动项目'
 
 nohup java -jar  -Xms128m -Xmx128m -Dspring.profiles.active=a1 /root/DiscoveryGuide/$name/target/$name-1.0.0.jar > log.out  2>&1 &
 
-echo "tail -f /root/logs/$name/info.log"
+echo "tail -f  /root/DiscoveryGuide/log.out"
 echo '脚本执行结束，请等待项目启动'
-tail -f /root/logs/$name/info.log
+tail -f /root/DiscoveryGuide/log.out
